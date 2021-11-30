275267c781bf9f629016dcb1f070e63da9006ab6
Rohith <karrarohithreddy@gmail.com>
2021-11-30 14:07:06 +0530

    initial

---
+++ src/app/model/wizard.spec.ts
@@ -0,0 +1,7 @@
+import { Wizard } from './wizard';
+
+describe('Wizard', () => {
+  it('should create an instance', () => {
+    expect(new Wizard()).toBeTruthy();
+  });
+});
---
+++ src/app/model/wizard.ts
@@ -0,0 +1,8 @@
+export class Wizard {
+    word:string
+    meaning:string
+    constructor(word:string,meaning:string){
+        this.word=word;
+        this.meaning=meaning;   
+    }
+}
